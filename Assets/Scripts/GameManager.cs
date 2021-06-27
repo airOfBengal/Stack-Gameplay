86cbda9f4c5101161f54c076221e235d981d90de
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2021-06-27 15:39:41 +0600

    Adds perfectly overlapped check

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject movingCube;
     [SerializeField] GameObject baseCube;
 
+    [SerializeField] float perfectionThreshold = 0.05f;
+
     public bool isLeftRightX = false;
     public bool isClickedToRun = false;
 
@@ -42,14 +44,26 @@ public class GameManager : MonoBehaviour
 
     public void SetMovingCube()
     {        
-        if (isClickedToRun && IsOverlapped())
+        if (isClickedToRun)
         {
-            Debug.Log("overlapped");
+            if (IsOverlapped())
+            {
+                
+                if (IsPerfectlyOverlapped())
+                {
+                    Debug.Log("perfectly overlapped");
+                }
+                else
+                {
+                    Debug.Log("overlapped");
+                }
+            }
+            else
+            {
+                Debug.Log("not overlapped");
+            }
         }
-        else
-        {
-            Debug.Log("not overlapped");
-        }
+       
 
         isClickedToRun = true;
         isLeftRightX = !isLeftRightX;
@@ -83,7 +97,7 @@ public class GameManager : MonoBehaviour
             float xLeft = movingCube.transform.position.x - movingCube.transform.localScale.x/2;
             float xRight = movingCube.transform.position.x + movingCube.transform.localScale.x/2;
             Debug.Log("xMin: " + xMin + " xMax: " + xMax + " xLeft: " + xLeft + " xRight: " + xRight);
-            if((xLeft > xMin && xLeft < xMax) || (xRight > xMin && xRight < xMax))
+            if((xLeft >= xMin && xLeft <= xMax) || (xRight >= xMin && xRight <= xMax))
             {
                 return true;
             }
@@ -95,7 +109,7 @@ public class GameManager : MonoBehaviour
             float zLeft = movingCube.transform.position.z - movingCube.transform.localScale.z/2;
             float zRight = movingCube.transform.position.z + movingCube.transform.localScale.z/2;
             Debug.Log("zMin: " + zMin + " zMax: " + zMax + " zLeft: " + zLeft + " zRight: " + zRight);
-            if ((zLeft > zMin && zLeft < zMax) || (zRight > zMin && zRight < zMax))
+            if ((zLeft >= zMin && zLeft <= zMax) || (zRight >= zMin && zRight <= zMax))
             {
                 return true;
             }
@@ -106,6 +120,30 @@ public class GameManager : MonoBehaviour
 
     bool IsPerfectlyOverlapped()
     {
+        if (isLeftRightX)
+        {
+            float xMin = baseCube.transform.position.x - baseCube.transform.localScale.x / 2;
+            //float xMax = baseCube.transform.position.x + baseCube.transform.localScale.x / 2;
+            float xLeft = movingCube.transform.position.x - movingCube.transform.localScale.x / 2;
+            //float xRight = movingCube.transform.position.x + movingCube.transform.localScale.x / 2;
+            //Debug.Log("xMin: " + xMin + " xMax: " + xMax + " xLeft: " + xLeft + " xRight: " + xRight);
+            if(Mathf.Abs(xMin - xLeft) <= perfectionThreshold)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            float zMin = baseCube.transform.position.z - baseCube.transform.localScale.z / 2;
+            //float zMax = baseCube.transform.position.z + baseCube.transform.localScale.z / 2;
+            float zLeft = movingCube.transform.position.z - movingCube.transform.localScale.z / 2;
+            //float zRight = movingCube.transform.position.z + movingCube.transform.localScale.z / 2;
+            //Debug.Log("zMin: " + zMin + " zMax: " + zMax + " zLeft: " + zLeft + " zRight: " + zRight);
+            if(Mathf.Abs(zMin - zLeft) <= perfectionThreshold)
+            {
+                return true;
+            }
+        }
         return false;
     }
 }
